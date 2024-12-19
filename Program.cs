7c3a9c5ad6453dd5398cc25090501f7c01569d36
Qvuark <roma18902015@gmail.com>
2024-12-20 00:28:53 +0200

    added search for files in folder(files with data as well)

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        List<string> files = new List<string>();
+        string directoryPath = @"C:\Users\roma1\source\repos\lab3oop\lab3oop";
+        for (int i = 10; i <= 29; i++)
+        {
+            string fileName = $"{i}.txt";
+            string filePath = Path.Combine(directoryPath, fileName);
+            files.Add(filePath);
+
+        }
+        List<string> noFile = new List<string>();
+        List<string> badData = new List<string>();
+        List<string> overflow = new List<string>();
+        List<int> validProducts = new List<int>();
+    }
+}
