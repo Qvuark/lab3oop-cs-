628ac859f8433579dbdf117c83d09dc7bb1596ae
Qvuark <roma18902015@gmail.com>
2024-12-20 00:38:58 +0200

    now program can read data and generate exceptions

--- Program.cs
+++ Program.cs
@@ -15,5 +15,64 @@
         List<string> badData = new List<string>();
         List<string> overflow = new List<string>();
         List<int> validProducts = new List<int>();
+
+        try
+        {
+            foreach (var file in files)
+            {
+                try
+                {
+                    string[] lines = File.ReadAllLines(file);
+                    try
+                    {
+                        string[] nums = new string[2];
+                        for (int i = 0; i < nums.Length; i++)
+                        {
+                            nums[i] = lines[i];
+                        }
+                        int a = ParseInt(nums[0]);
+                        int b = ParseInt(nums[1]);
+                        checked
+                        {
+                            int product = a * b;
+                            validProducts.Add(product);
+                        }
+                    }
+                    catch (Exception ex) when (ex is IndexOutOfRangeException || ex is ArgumentNullException)
+                    {
+                        badData.Add(file);
+                    }
+                }
+                catch (FileNotFoundException)
+                {
+                    noFile.Add(file);
+                }
+                catch (FormatException)
+                {
+                    badData.Add(file);
+                }
+                catch (OverflowException)
+                {
+                    overflow.Add(file);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: Unable to create or write to output files. " + ex.Message);
+        }
+    }
+    static int ParseInt(string line)
+    {
+        int result;
+        try
+        {
+            result = int.Parse(line);
+        }
+        catch (FormatException)
+        {
+            throw new FormatException("Line is not a valid integer.");
+        }
+        return result;
     }
 }
